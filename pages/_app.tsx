efe25e2aefec10dc7063059ad3769f0cbf98e63f
Sakihiko-03 <lek17622@gmail.com>
2022-11-06 13:22:43 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // <ThemeProvider defaultTheme='dark'>
+    <ThemeProvider enableSystem={true} attribute='class'>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+  // <Component {...pageProps} />
 }
 
 export default MyApp
